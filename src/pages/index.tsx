0e59580db42be39b55b6e37720a4cf1f58738dc9
Marcos Duarte <marcosduarte1994@gmail.com>
2023-02-08 22:21:03 -0300

    feat: create home at app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,31 @@
-import { styled } from '../styles'
+import Image from 'next/image'
+import { HomeContainer, Product } from '../styles/pages/home'
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  color: 'white',
-  borderRadius: 4,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: 'bold',
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
+import camiseta1 from '../assets/Shirt/1.png'
+import camiseta2 from '../assets/Shirt/2.png'
+import camiseta3 from '../assets/Shirt/3.png'
+import camiseta4 from '../assets/Shirt/4.png'
 
 export default function Home() {
   return (
-    <Button>
-      <span>Button</span>
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,00</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,00</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
