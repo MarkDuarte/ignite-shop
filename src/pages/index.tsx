4b246ee917ca701bb6e9eba9e9a221b833130103
Marcos Duarte <marcosduarte1994@gmail.com>
2023-02-09 23:22:19 -0300

    use get server side props in next js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ import camiseta4 from '../assets/Shirt/4.png'
 
 import 'keen-slider/keen-slider.min.css'
 
-export default function Home() {
+export default function Home(props) {
   const [slideRef] = useKeenSlider({
     slides: {
       perView: 3,
@@ -20,6 +20,7 @@ export default function Home() {
   })
   return (
     <HomeContainer ref={slideRef} className="keen-slider">
+      <pre>{JSON.stringify(props.list)}</pre>
       <Product className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt="" />
 
@@ -58,4 +59,13 @@ export default function Home() {
 
     </HomeContainer>
   )
+}
+
+export const getServerSideProps = async () => {
+  await new Promise(resolve => setTimeout(resolve, 2000))
+  return {
+    props: {
+      list: [1, 2, 3]
+    }
+  }
 }
