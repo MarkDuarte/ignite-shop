27385f0b025ab2873c843be6716709a907c3cc61
Marcos Duarte <marcosduarte1994@gmail.com>
2023-02-15 15:25:34 -0300

    feat: add in api getstatic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Image from 'next/image'
-import Stripe from 'stripe'
+
 
 import { useKeenSlider } from 'keen-slider/react'
 
+import Stripe from 'stripe'
 import { HomeContainer, Product } from '../styles/pages/home'
-import { stripe } from '../lib/api'
 
+import { stripe } from '../lib/api'
 import { formatterCurrency } from '../utils/formatterCurrency'
 
 import 'keen-slider/keen-slider.min.css'
@@ -45,7 +46,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -64,6 +65,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2,
   }
 }
