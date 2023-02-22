dbf2e6a390784dfae15fb5063094c87593e9da5b
Marcos Duarte <marcosduarte1994@gmail.com>
2023-02-21 21:05:20 -0300

    feat: create layout screen success

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,23 @@
+import Link from "next/link";
+import { ImageContainer } from "../styles/pages/success";
+import { SuccessContainer } from "../styles/pages/success";
+
 export default function Success() {
   return (
-    <h1>Success</h1>
+    <SuccessContainer>
+      <h1>Compra Efetuada</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhuul <strong>Marcos Duarte</strong>, sua <strong>Camiseta Beyond the Limits</strong> já a caminho de sua casa. 
+      </p>
+
+      <Link href="/">
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
   )
 }
